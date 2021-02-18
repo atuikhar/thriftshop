295bdb4bf79c762764158c6b827a4a4897d10dbf
che000 <atuikhar@gmail.com>
2021-02-18 17:27:48 +0530

    ok

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
 const adminProductsRouter = require('./routes/admin/products');
@@ -8,8 +7,13 @@ const cartsRouter = require('./routes/carts');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cookieSession({
     keys: ['lkasld235j']
