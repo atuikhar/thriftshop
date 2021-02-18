c0fa3a17aeba562d6bc0aefe19ed40d4b7201381
che000 <atuikhar@gmail.com>
2021-02-18 18:56:58 +0530

    ok

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
 const adminProductsRouter = require('./routes/admin/products');
